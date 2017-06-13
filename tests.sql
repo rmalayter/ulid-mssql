bfd9793ff4f34058d3cfd7f55472628babbec891
Ryan Malayter <rmalayter@bai.org>
2017-06-13 12:05:14 -0500

    initial tests commit

---
+++ tests.sql
@@ -0,0 +1,101 @@
+/* tests for ulid functions */
+SET NOCOUNT ON
+
+DECLARE @st DATETIME2(7)
+DECLARE @et DATETIME2(7)
+DECLARE @c INT
+DECLARE @cmax INT
+DECLARE @frag FLOAT
+
+--number of rows used in tests
+SET @cmax = 100000
+
+--ulid as UNIQUEIDENTIFIER test
+CREATE TABLE #t (
+	pk UNIQUEIDENTIFIER PRIMARY KEY CLUSTERED
+	,d NVARCHAR(1000)
+	)
+
+SET @c = 0
+SET @st = SYSUTCDATETIME()
+
+WHILE @c < @cmax
+BEGIN
+	INSERT INTO #t (
+		pk
+		,d
+		)
+	VALUES (
+		dbo.ulid()
+		,N'dummy data for testing purposes; this should be a realistic length for narrow tables.'
+		)
+
+	SET @c = @c + 1
+END
+
+SET @et = SYSUTCDATETIME()
+SET @frag = (SELECT TOP 1 CAST(avg_fragmentation_in_percent as varchar(100)) FROM sys.dm_db_index_physical_stats(DB_ID(),OBJECT_ID(N'#t'),NULL,NULL,NULL))
+PRINT 'ulid() as primary key INSERTION TEST: ' 
+PRINT '     rows/sec: ' + CAST(CAST(@cmax AS FLOAT) * 1000 / DATEDIFF(ms, @st, @et) AS VARCHAR(100))
+PRINT '     avg_fragmentation_in_percent: ' + CAST(@frag as varchar(100))
+DROP TABLE #t
+
+--newid as UNIQUEIDENTIFIER test
+CREATE TABLE #t2 (
+	pk UNIQUEIDENTIFIER PRIMARY KEY CLUSTERED
+	,d NVARCHAR(1000)
+	)
+
+SET @c = 0
+SET @st = SYSUTCDATETIME()
+
+WHILE @c < @cmax
+BEGIN
+	INSERT INTO #t2 (
+		pk
+		,d
+		)
+	VALUES (
+		NEWID()
+		,N'dummy data for testing purposes; this should be a realistic length for narrow tables.'
+		)
+
+	SET @c = @c + 1
+END
+
+SET @et = SYSUTCDATETIME()
+SET @frag = (SELECT TOP 1 CAST(avg_fragmentation_in_percent as varchar(100)) FROM sys.dm_db_index_physical_stats(DB_ID(),OBJECT_ID(N'#t2'),NULL,NULL,NULL))
+PRINT 'newid() as primary key INSERTION TEST: '
+PRINT '     rows/sec: ' + CASt(CAST(@cmax AS FLOAT) * 1000 / DATEDIFF(ms, @st, @et) AS VARCHAR(100))
+PRINT '     avg_fragmentation_in_percent: ' + CAST(@frag as varchar(100))
+DROP TABLE #t2
+
+--newsequentialID as UNIQUEIDENTIFIER test
+CREATE TABLE #t3 (
+	pk UNIQUEIDENTIFIER PRIMARY KEY CLUSTERED DEFAULT (newsequentialid())
+	,d NVARCHAR(1000)
+	)
+
+SET @c = 0
+SET @st = SYSUTCDATETIME()
+
+WHILE @c < @cmax
+BEGIN
+	INSERT INTO #t3 (
+		pk
+		,d
+		)
+	VALUES (
+		DEFAULT
+		,N'dummy data for testing purposes; this should be a realistic length for narrow tables.'
+		)
+
+	SET @c = @c + 1
+END
+
+SET @et = SYSUTCDATETIME()
+SET @frag = (SELECT TOP 1 CAST(avg_fragmentation_in_percent as varchar(100)) FROM sys.dm_db_index_physical_stats(DB_ID(),OBJECT_ID(N'#t2'),NULL,NULL,NULL))
+PRINT 'newsquentialid() as primary key INSERTION TEST: '
+PRINT '     rows/sec: ' + CASt(CAST(@cmax AS FLOAT) * 1000 / DATEDIFF(ms, @st, @et) AS VARCHAR(100))
+PRINT '     avg_fragmentation_in_percent: ' + CAST(@frag as varchar(100))
+DROP TABLE #t3
